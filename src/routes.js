94d7c37ddbe7f502c7dee34f516376ad7f3e8dfc
kumar <krksingh.99@gmail.com>
2024-08-17 15:05:34 +0530

    show and stared page added

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import React, { lazy } from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './page/Home/Home';
 import { HomePageLayout, LazlyLoad } from './component';
+import Show from './page/Show/Show.jsx';
+import Starred from './page/Stared/Stared.jsx';
 const UnknownPage = lazy(() =>import('./page/Unknown/Unknown.jsx'));
 
 const MovieRoutes = () => {
@@ -11,6 +13,16 @@ const MovieRoutes = () => {
     <Routes>
       <Route  index element={<HomePageLayout><Home/></HomePageLayout>}/>
       <Route path='/home' element={<HomePageLayout><Home/></HomePageLayout>}/>
+      <Route path='/show/:id' element={
+        <LazlyLoad>
+            <Show />
+        </LazlyLoad>
+      }/>
+      <Route path='/stared' element={
+        <LazlyLoad>
+            <Starred />
+        </LazlyLoad>
+      }/>
       <Route path='*' element={
         <LazlyLoad>
             <UnknownPage />
